4ab5ee0f71b94956c9ff4dfef6b6bfd547a9e29f
vuongthai1205 <2051010367vuong@ou.edu.vn>
2023-06-19 22:24:24 +0700

    third commit

---
+++ app/src/main/java/com/tgv/app/BT7Activity.java
@@ -0,0 +1,14 @@
+package com.tgv.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BT7Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bt7);
+    }
+}
